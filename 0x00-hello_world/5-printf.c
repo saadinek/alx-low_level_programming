c14e449267994ffe32a876dc04be095ae1643cba
saadinek <saadinek@gmail.com>
2023-02-16 17:01:50 -0800

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Printing a line with printf fonction
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+
