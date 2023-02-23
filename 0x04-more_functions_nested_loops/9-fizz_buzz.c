21c0428907ab2e7f1426c45f2692331559a11675
saadinek <saadinek@gmail.com>
2023-02-23 15:54:38 -0800

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -20,3 +20,18 @@ int main(void)
 		{
 			printf(" Buzz");
 		} else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		} else if (i == 1)
+		{
+			printf("%d", i);
+		} else
+		{
+			printf(" %d", i);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
+
